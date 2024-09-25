f390739523aa1a0d43e2c86ca24ebfac681b5605
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-09-25 17:36:57 +0100

    Add simple button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,9 @@
-export function Button() {
-  return <button>Button</button>;
+import { ReactNode } from "react";
+
+type ButtonProps = {
+  children: ReactNode;
+};
+
+export function Button({ children }: ButtonProps) {
+  return <button>{children}</button>;
 }
