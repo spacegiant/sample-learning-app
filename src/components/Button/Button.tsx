b5a88bcb69718c68983df1931f663e14e33dcf0e
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-09-26 19:51:29 +0100

    Add PaginationButton for footer

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,40 @@
-import { ReactNode } from "react";
+import { JSX, ReactNode } from "react";
 
 type ButtonProps = {
   children: ReactNode;
+  disabled?: boolean;
 };
 
-export function Button({ children }: ButtonProps) {
-  return <button>{children}</button>;
+export function Button({ children, disabled }: ButtonProps) {
+  const classes = `${
+    disabled && "opacity-30"
+  } bg-lime-700 hover:bg-lime-800 text-lime-200 px-4 py-2 rounded-md`;
+  return (
+    <button disabled={disabled} className={classes}>
+      {children}
+    </button>
+  );
 }
+
+type PaginationButtonProps = {
+  isRight?: boolean;
+};
+
+export function PaginationButton({
+  children,
+  disabled,
+  isRight,
+}: ButtonProps & PaginationButtonProps) {
+  const classes = `${disabled && "opacity-30"} ${
+    isRight ? "rounded-l-none" : "rounded-r-none"
+  } bg-lime-700 hover:bg-lime-800 text-lime-200 px-4 py-2 rounded-md`;
+  return (
+    <button disabled={disabled} className={classes}>
+      {children}
+    </button>
+  );
+}
+
+export const ActionButton = (props: JSX.IntrinsicAttributes & ButtonProps) => {
+  return <Button {...props}></Button>;
+};
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Button } from "..";
+import { PaginationButton } from "../Button/Button";
 
 type FooterProps = {
   currentPage: number;
@@ -8,10 +8,16 @@ type FooterProps = {
 export function Footer({ currentPage, totalPages }: FooterProps) {
   const progressDisplay = `Page ${currentPage} / ${totalPages}`;
   return (
-    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-end opacity-30 hover:opacity-100">
-      <Button>Back</Button>
-      <span>{progressDisplay}</span>
-      <Button>Forward</Button>
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-end">
+      <div className="rounded-md bg-slate-300">
+        <PaginationButton>
+          ←<span className="sr-only">Go to previous page</span>
+        </PaginationButton>
+        <span className="px-4">{progressDisplay}</span>
+        <PaginationButton isRight>
+          →<span className="sr-only">Go to next page</span>
+        </PaginationButton>
+      </div>
     </footer>
   );
 }
