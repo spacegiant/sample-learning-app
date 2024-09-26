35550e73fab658b9ac154c482b4f06e6973305c5
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-09-26 17:35:20 +0100

    Styling tweaks

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ type FooterProps = {
 export function Footer({ currentPage, totalPages }: FooterProps) {
   const progressDisplay = `Page ${currentPage} / ${totalPages}`;
   return (
-    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-end opacity-30 hover:opacity-100">
       <Button>Back</Button>
       <span>{progressDisplay}</span>
       <Button>Forward</Button>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,8 @@
 export function Header(props: { courseTitle: string }) {
   return (
-    <header className="flex justify-between items-start border-b-2 border-solid border-blue-400">
+    <header className="flex justify-between items-start border-b-2 border-solid border-slate-300 text-slate-500">
       <span className="flex-1">Sample Learning App</span>
-      <span className="flex-1 text-center">{props.courseTitle}</span>
+      <span className="flex-1 text-center font-bold">{props.courseTitle}</span>
       <span className="flex-1"></span>
     </header>
   );
