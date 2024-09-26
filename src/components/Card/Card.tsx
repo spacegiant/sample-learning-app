d0f9b0b509fad0f0e9c04b2155648789350a46a6
spacegiant <599280+spacegiant@users.noreply.github.com>
2024-09-26 19:50:50 +0100

    Put max width on card text content

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 function Card(props: { children: ReactNode; classNames?: string }) {
   const classes = `${
     props.classNames ?? ""
-  } px-8 py-4 rounded-md text-black min-w-52 min-h-36 inline-block flex items-center justify-center `;
+  } px-8 py-4 rounded-md text-black min-w-52 min-h-36 inline-block flex items-center justify-center max-w-2 text-center`;
   return <span className={classes}>{props.children}</span>;
 }
 
